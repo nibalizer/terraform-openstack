f3b76ece2d4f52b2f1f345b54a59cae8efe0514e
Spencer Krum <nibz@spencerkrum.com>
2016-05-28 17:58:21 -0700

    Basic openstack terraform files

---
+++ instance_a.tf
@@ -0,0 +1,21 @@
+# Configure the OpenStack Provider
+provider "openstack" {
+    user_name  = "${var.user_name}"
+    tenant_name = "${var.tenant_name}"
+    password  = "${var.password}"
+    auth_url  = "${var.auth_url}"
+}
+
+resource "openstack_compute_instance_v2" "basic" {
+  name = "basic"
+  image_id = "e45c00df-b189-414c-b82c-0917cd8a464d"
+  flavor_id = "small"
+  key_pair = "${var.key_pair_name}"
+  security_groups = ["default"]
+
+  network {
+    name = "external"
+  }
+
+}
+
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "user_name" {}
+variable "tenant_name" {}
+variable "password" {}
+variable "auth_url" {}
+variable "key_pair_name" {}
