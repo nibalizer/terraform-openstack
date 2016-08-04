154c4383117114478d9bc98bbd9f711dc030a220
CullenTaylor <mctaylor@us.ibm.com>
2016-08-04 15:57:30 -0500

    add new variables + cleanup
    This adds three variables that can be used to overwrite
    flavor id, image id, and security groups. That way
    more people can use the repo without having to edit
    instance_a.tf. This has (of course) been tested.

--- instance_a.tf
+++ instance_a.tf
@@ -1,19 +1,19 @@
 # Configure the OpenStack Provider
 provider "openstack" {
-    user_name  = "${var.user_name}"
+    user_name   = "${var.user_name}"
     tenant_name = "${var.tenant_name}"
-    password  = "${var.password}"
-    auth_url  = "${var.auth_url}"
+    password    = "${var.password}"
+    auth_url    = "${var.auth_url}"
     domain_name = "${var.domain_name}"
 }
 
 resource "openstack_compute_instance_v2" "terraform-server" {
-  count = 8
-  name = "terraform-server"
-  image_id = "ec415914-6caa-428e-b72f-77a655342ffc"
-  flavor_id = "f4c1349e-59bf-47af-8960-be65c781d792"
-  key_pair = "${var.key_pair_name}"
-  security_groups = ["default"]
+  count           = 8
+  name            = "terraform-server-${count.index + 1}"
+  image_id        = "${var.image_id}"
+  flavor_id       = "${var.flavor_id}"
+  key_pair        = "${var.key_pair_name}"
+  security_groups = ["${var.security_groups}"]
 
   network {
     name = "provider"
